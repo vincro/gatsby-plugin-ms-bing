971e62da18c6c87fec415174726c10f07340cbb8
Vincent Roman <vincent@delamainit.com>
2021-09-29 21:45:26 +0100

    feat(ms-bing-tracking): Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # gatsby-plugin-ms-bing
+
 Gatsby plugin for adding Microsoft Bing tracking code
+
+## Install
+
+`npm install --save gatsby-plugin-ms-bing`
+
+## How to use
+
+```javascript
+// In your gatsby-config.js
+plugins: [
+  {
+    resolve: `gatsby-plugin-ms-bing`,
+    options: {
+      bingId: "__ID__HERE__",
+    },
+  },
+];
+```
---
+++ src/gatsby-ssr.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+    if (
+        process.env.NODE_ENV === `production` &&
+        pluginOptions &&
+        pluginOptions.bingId
+    ) {
+        return setHeadComponents([
+            <script
+                data-obct
+                type="text/javascript"
+                key={`gatsby-plugin-ms-bing`}
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"${pluginOptions.bingId}"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
+                    `,
+                }}
+            />,
+        ]);
+    }
+};
+
+
